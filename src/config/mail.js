3880a34bbb2236cd8055750fe0c67a2e5452847f
Régis de Oliveira <regis_crysis@hotmail.com>
2020-05-17 19:14:04 -0300

    Config nodemailer to send emails using Mailtrap

---
+++ src/config/mail.js
@@ -0,0 +1,14 @@
+export default {
+  // Mailtrap (DEV ambient)
+  // Amazon SES to online applications//
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '4577bf1cb0fdc4',
+    pass: '0c2b6b19b80d91',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
---
+++ src/lib/Mail.js
@@ -0,0 +1,27 @@
+import nodemailer from 'nodemailer';
+import mailConfig from '../config/mail';
+
+class Mail {
+  constructor() {
+    const { host, port, secure, auth } = mailConfig;
+
+    this.transporter = nodemailer.createTransport({
+      host,
+      port,
+      secure,
+      /**
+       * Verifica se dentro de auth existe um usuario, se nao passa como nulo
+       */
+      auth: auth.user ? auth : null,
+    });
+  }
+
+  sendMail(message) {
+    return this.transporter.sendMail({
+      ...mailConfig.default,
+      ...message,
+    });
+  }
+}
+
+export default new Mail();
